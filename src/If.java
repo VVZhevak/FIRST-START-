e8f616a00628045910bba4a37abb8b55bf8abe2e
unknown <adminus@RU621-WHP-04.corp.iu.expert>
2019-12-13 11:12:56 +0300

    second commit

--- src/If.java
+++ src/If.java
@@ -2,7 +2,7 @@ public class If {
     public static void main(String[] args) {
         int myint=5;
         if (myint<10) {
-          System.out.println("да, верно");
+          System.out.println("да, верно, все отлично");
         } else if (myint>20) {
             System.out.println("нет, не верно");
         } else
